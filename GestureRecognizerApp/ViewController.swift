50dad29d349d75f80125140a8c69f5780a98b188
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-15 21:37:26 +0300

    first commit
    A simple application to understand the basics of gesture recognizer

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isTrilogy = true
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func changePic() {
+        if isTrilogy == true{
+            
+            imageView.image = UIImage(named: "weekndkissland")
+            myLabel.text = "Kiss Land"
+            isTrilogy = false
+        }else{
+            
+            imageView.image = UIImage(named: "Trilogy")
+            myLabel.text = "Trilogy"
+            isTrilogy = true
+        }
+        
+
+        
     }
 
-
 }
-
