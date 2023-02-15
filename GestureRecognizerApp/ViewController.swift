80ec69cb089cd40d7d1a7f9932de7676cfc9d5d5
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-15 20:40:28 +0300

    Initial Commit

---
+++ GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Mertcan Ünek on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
